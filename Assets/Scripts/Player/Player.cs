a78f02676d58bf492cefc20fbcfe789ad8d0cebf
Ricardo <rmachadobdev@gmail.com>
2023-02-02 14:37:36 -0300

    jump implemented

--- Assets/Scripts/Player/Player.cs
+++ Assets/Scripts/Player/Player.cs
@@ -6,21 +6,45 @@ public class Player : MonoBehaviour
 {
     public Rigidbody2D myRigidbody;
 
-    public Vector2 velocity;
-    
+    public Vector2 friction = new Vector2(.1f, 0);
+
     public float speed;
-    void Update()
+    public float forceJump = 30;
+
+    private void Update()
+    {
+        HandleJump();
+        HandleMovement();
+    }
+    private void HandleMovement()
     {
         if (Input.GetKey(KeyCode.LeftArrow))
         {
-           //  myRigidbody.MovePosition(myRigidbody.position - velocity * Time.deltaTime);
             myRigidbody.velocity = new Vector2(-speed, myRigidbody.velocity.y);
         }
         else if (Input.GetKey(KeyCode.RightArrow))
         {
-            //myRigidbody.MovePosition(myRigidbody.position + velocity * Time.deltaTime);
             myRigidbody.velocity = new Vector2(speed, myRigidbody.velocity.y);
+        }
 
+        if (myRigidbody.velocity.x > 0)
+        {
+            myRigidbody.velocity += friction;
+        }
+        else if (myRigidbody.velocity.x < 0)
+        {
+            myRigidbody.velocity -= friction;
+        }
+
+    }
+
+    private void HandleJump()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            myRigidbody.velocity = Vector2.up * forceJump;
         }
     }
+
+
 }
